6ca27055dba664d60f61d6755796ee2e1f93e5eb
Andreas Thöni <andi.thoeni@gmail.com>
2026-03-29 10:49:58 +0200

    pre-regulated commit

--- ros2_turtlectrl/src/py_turtlectrl/py_turtlectrl/turtle_ctrl.py
+++ ros2_turtlectrl/src/py_turtlectrl/py_turtlectrl/turtle_ctrl.py
@@ -4,12 +4,18 @@ from rclpy.node import Node
 
 # import message type "String", other are "Pose", "Twist", "Quaternion", ...
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
+from nav_msgs.msg import Odometry
+from tf_transformations import euler_from_quaternion
 
 import numpy as np
 #-------------------------------------------------------------------------------#
 # THese dependencies ^ need to be added to "package.xml" !!
 
+class simplePose:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
 
 # Pub Node setup, inehrit from "Node"
 #-------------------------------------------------------------------------------#
@@ -18,29 +24,39 @@ class PurePursuit(Node):
         super().__init__('pure_pursuit_node') # NOde name, call Node constructor
 
         # params
-        self.look_ahead_distance = 1
-        self.linear_vel = 1.5
+        self.look_ahead_distance = 0.1
+        self.linear_vel = 0.11
 
         # test path (Max aktiv)
-        self.path = [(5.0, 5.0), (3.0, 2.0), (2.0, 7.0), (10.0, 9.0), (7.0, 4.0)]
+        self.path = [(0.25, 0.25), (0.5, 1.0), (1.0, 0.75), (0.75, 0.5), (1.0, 0.25)]
         self.target_idx =  0
 
         # publisher and subscriber member for Pose and Twist of turtles
-        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+        self.publisher_ = self.create_publisher(Twist, '/my_robot/cmd_vel', 10)
+        self.subscription = self.create_subscription(Odometry, '/my_robot/odom', self.pose_callback, 10)
 
 
     def pose_callback(self, msg):
+        point = msg.pose.pose.position
+        orient_q = msg.pose.pose.orientation
+        orient_list = [orient_q.x, orient_q.y, orient_q.z, orient_q.w]
+
+        (roll, pitch, yaw) = euler_from_quaternion(orient_list)
+        
+        pose = simplePose(point.x, point.y, yaw)
+
+
         # 1) find target waypoint
-        self.update_target(msg)
+        self.update_target(pose)
         target_x, target_y = self.path[self.target_idx]
 
         # 2) transform target to turtlinger coords
-        dx = target_x - msg.x
-        dy = target_y - msg.y
+
+        dx = target_x - pose.x
+        dy = target_y - pose.y
 
         # hom rot mat
-        y_local = -dx * np.sin(msg.theta) + dy * np.cos(msg.theta)
+        y_local = -dx * np.sin(pose.theta) + dy * np.cos(pose.theta)
 
         # 3) pure pursuit algo
         # curvature gamma = 2 * y_local / L^2
@@ -49,7 +65,9 @@ class PurePursuit(Node):
         # 4) publish twist
         cmd = Twist()
         cmd.linear.x = self.linear_vel
-        cmd.angular.z = self.linear_vel * curvature
+        
+        max_ang = 2.0  # rad/s
+        cmd.angular.z = np.clip(self.linear_vel * curvature, -max_ang, max_ang)
         self.publisher_.publish(cmd)
 
     def update_target(self, pose):
